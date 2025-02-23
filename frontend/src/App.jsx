3adafce94981b88f151989c1389b96c048fde5e4
dead-shadow-7 <deadshadow12119@gmail.com>
2025-02-23 15:25:42 +0530

    Reverted back the changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import Home from "./pages/Home";
+import ReactGA from "react-ga4";
+const measurementId = "G-EPF21ZKEHC";
+ReactGA.initialize(measurementId);
+ReactGA.send("pageview");
 
 const App = () => {
   return (
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,17 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/global.css";
-import ReactGA from "react-ga4";
-
-// Initialize GA4 with your measurement ID
-ReactGA.initialize("G-EPF21ZKEHC");
-
-// Optional: Enable debug mode during development
-if (process.env.NODE_ENV === "development") {
-  ReactGA.set({
-    debug: true,
-  });
-}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
