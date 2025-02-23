73cb1a99ee13f0ede11787f22efa14def758b85e
dead-shadow-7 <deadshadow12119@gmail.com>
2025-02-23 14:52:23 +0530

    Added google analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/global.css";
+import ReactGA from "react-ga4";
+
+const measurementId = import.meta.env.VITE_MEASUREMENT_ID;
+
+// Initialize Google Analytics
+ReactGA.initialize(measurementId);
+
+const Root = () => {
+  useEffect(() => {
+    ReactGA.send("pageview"); // Track the initial page load
+  }, []);
+
+  return <App />;
+};
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );
