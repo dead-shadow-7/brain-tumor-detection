c89807daf3b2980f2865ac97a7dbf438384f0073
dead-shadow-7 <deadshadow12119@gmail.com>
2025-02-23 15:31:53 +0530

    Some more reverts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,5 @@
 import React from "react";
 import Home from "./pages/Home";
-import ReactGA from "react-ga4";
-const measurementId = "G-EPF21ZKEHC";
-ReactGA.initialize(measurementId);
-ReactGA.send("pageview");
-
 const App = () => {
   return (
     <div className="app">
