8feddd55a90315aec16de133bff67a0b8f4baefa
dead-shadow-7 <deadshadow12119@gmail.com>
2025-02-28 19:40:51 +0530

    Route Path added for technology and contact

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Technology from "./pages/Technology";
+import Contact from "./pages/Contact";
 import PredictionPage from "./pages/PredictionPage";
 import Navbar from "./components/NavBar";
 import PageNotFound from "./pages/PageNotFound";
@@ -16,6 +18,8 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/technology" element={<Technology />} />
+          <Route path="/contact" element={<Contact />} />
           <Route path="/predict" element={<PredictionPage />} />
           <Route path="/login" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
