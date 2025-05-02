a6d69ce65fd8bd3252840dfcc7adb0950021c1be
dead-shadow-7 <deadshadow12119@gmail.com>
2025-05-02 19:41:04 +0530

    Added email in contact us

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -3,7 +3,10 @@ const Contact = () => {
   return (
     <div className="contact">
       <h1>Contact Us</h1>
-      <div className="description">For any queries, please contact us.</div>
+      <div className="description">
+        For any queries, please contact us at mail{" "}
+        <b>shivamrajput.dev@proton.me</b>
+      </div>
     </div>
   );
 };
